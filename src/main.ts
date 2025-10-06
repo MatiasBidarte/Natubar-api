6d8ab4230bee20ccfeb9e7d0af81df3eef6d51fe
RafaPedetti <107689601+RafaPedetti@users.noreply.github.com>
2025-10-06 17:58:59 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://www.natubar.com.uy', 'http://localhost:3000'],
+    origin: ['https://www.natubar.com.uy', 'https://natubar.vercel.app' , 'http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
     credentials: true,
